388071f1e1546b0ebc59fb7be141fa319792c233
Doug Ritchie <dritchie3@sbcglobal.net>
2020-02-19 12:09:43 -0600

    clean read.me

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var testQuestion = [
     {
     question: "The German Purity law of 1560, requires that Beer be made from all but what ingredient?",
     answers: ["Yeast","Hops","Malt","Corn"],
-    answer: 4 },
+    answer: "Yeast" },
     {
-    question: "Some types of beer are named all but what?",
+    question: "All are various beer styles but what?",
     answers: ["Porter", "Dunkirk", "Kolsch", "Lambic"],
     answer: 2
     }, 
@@ -15,7 +15,7 @@ var testQuestion = [
     answer: 1
     },
     {
-    question: "What is a reason Beer can be drunk?",
+    question: "What is a reason to drink Beer?",
     answers: ["Just for Fun", "Celebrations", "Washing Down Meals", "All of the Above"],
     answer: 4
     },
@@ -26,103 +26,115 @@ var testQuestion = [
     }
 ]
 
+//timer start and other  variables
+var pageEl = document.getElementById('page');
+var title = 'Beer Quiz Challange';
+var directions = 'Try and answer the Beer related questions within the time limit. Keep in mind that a wrong answer will penalize your Scoretime by 10 seconds. You will get 10 points for every correct answer and 1 point for every second left after the test.';
 var answerEl = document.querySelector("#answers");
 var questionEl = document.querySelector("#question");
-var corAnswerEl = testQuestion.answer;
-console.log(corAnswerEl);
-console.log(testQuestion);
-
-//timer start and other  variables 
-var timer = document.getElementById("timer");
-var timerEl;
+var timerEl = document.getElementById("timer");
 var buttonStart = document.getElementById("btn-startQuiz");
 var startPage = document.getElementById("start");
 var count = 75;
 var score = 0;
+var t = 0;
+var k = 0;
+var q = 0;
 
-var correctAns = testQuestion[0].answer;
-// var btn = document.getElementById();
-console.log(correctAns);
+var titleEl = document.getElementById('title').append(title);
+var directionsEl = document.querySelector('#Directions').append(directions);
 
-// for (var g = 0; g < testQuestion[0].length; g++){
-//     btn.addEventListener("click", clickAns()) 
-    //start timer
+//start timer
     buttonStart.addEventListener("click", function() {
-        timerEl = setInterval(function() {
-            console.log(count)
+        timer = setInterval(function() {
+            // console.log(count)
             count--;
-            timer.textContent = count;
+            timerEl.textContent = count;            
             //stoping timer when needed
-            if(count === 72) {
-                clearInterval(timerEl);  
+            if (count === 0) {
+                clearInterval(timer);  
                 console.log("interval has been cleared")          
             }
-            }, 1000);            
-                
-    //correct item clicked function
+            }, 1000);         
+});
 
-            function btnClicked(){
-                // btn.addEventListener("click", clickAns()) 
+document.addEventListener("click", function() {
+    var parent = document.getElementById("page").getElementsByTagName("div")[0];
+    var child = document.getElementsByTagName('div')[0];
+    var removed = document.remove(child);
+
+    renderQuestions();
+});
+
+document.addEventListener('click', function(){
+    k++;    
+});
+
+//Start asking question and answers            
+    function renderQuestions() { 
+    //     var parent = document.getElementById('page');
+    //     var newEl1 = document.createElement('div');
+    //     var newEl2 = document.createElement('h3');
+    //     var newEl3 = document.createElement('ol');
+
+    //    console.log(page)
+    //     var mainDiv = document.getElementById("page");
+    //     var parent = document.createElement("div");
+    //     var child = document.createElement('h3');
+
+
+        
+        // var ques = child.createElement('h3');
+       
+        // var child = document.createElement('h3').textContent = testQuestion[k].question;
+        // document.textContent = testQuestion[k].question;
+        console.log('k = ' + k);
+        // document.appendchild(parent);
+        // var parent = document.createElement('div');
+
+
+        var questionCreate = document.createElement('h3')
+        questionCreate.textContent = testQuestion[k].question;
+        questionEl.append(questionCreate);
+
+        // console.log('this is the page div ' + pageEl);
+       
+  
+        
+        
+//print questions and answers. Add click ability to answers.          
+        for (var i = 0; i < testQuestion[0].answers.length; i++) {
+            var answerCreate = document.createElement('li');  
+            var br = document.createElement('br');
+            var a = document.createElement('button'); 
+            a.setAttribute('id', 'corAns1');  
+            a.textContent = testQuestion[k].answers[i];                        
+            q++
+            t++
+            answerCreate.setAttribute('class', 'corAns');                        
+            answerEl.append(br);                        
+            answerEl.append(answerCreate);
+            answerCreate.append(a);  
             
+            console.log('t = ' + t);                     
+            console.log('i = ' + i);
+            console.log('li = ' + q);
+            console.log('answerCreate = ' + answerCreate);
+            console.log(' answers length is = ' + testQuestion[i].answers.length);                  
+            console.log('correct answer = ' + testQuestion[i].answer);
+        };                
+        return;
+            
+    };
 
-        
-                function clickAns() {
-                    if (correctAns = li) {
-                        renderQuestions();
-                    }
-                    else btnClicked()
-                    
-                };
-            };
+   
 
 
-        
-    //Start asking question and answers            
-            function renderQuestions() {
 
-                for (var k = 0; k < testQuestion[0].question.length; k++) {
-                    var questionCreate = document.createElement('h3');
-                    questionCreate.textContent = testQuestion[k].question;
-                    questionEl.append(questionCreate);
-                    var q = 0;
-                    console.log(questionEl);
-                    console.log(questionCreate);
-                    
-                    for (var i = 0; i < testQuestion[0].answers.length; i++) {
-                        var answerCreate = document.createElement('li');  
-                        var br = document.createElement('br');
-                        var a = document.createElement('a'); 
-                        a.setAttribute('href', "http://google.com");  
-                        // a.href = 'http://google.com'              
-                        a.textContent = testQuestion[0].answers[i];                        
-                        q++
-                        answerCreate.setAttribute('class', q);                        
-                        answerEl.append(br);
-                        
-                        answerEl.append(answerCreate);
-                        answerCreate.append(a);
-                        
-                        console.log(answerCreate);
-                        console.log(answerEl);
-                        console.log(testQuestion[0].answers[i]);
-                    };
-                    btnClicked();
-                    return questionCreate
-                };
-        
-            };
 
-            renderQuestions();
-            btnClicked();
 
-    });
-// };    
-//when High score text is clicked, go to high score page
-
-//print questions and answers
-//add submit on answers
-//when answer is clicked show "right" or "wrong" ant bottom 
-//change scor 10 points for right answer and -10 for wrong answer
+//when answer is clicked show "right" or "wrong" answer on bottom 
+//change score 10 points for right answer and -10 for wrong answer
 //create button to continue
 //when question is chosen move to next question
 //when all questions are answers-->
